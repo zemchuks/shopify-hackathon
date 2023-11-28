5fc2f5f9802cf0287593e2ef064f5c7e9f2ed47e
bruno <brunoaugustine2@gmail.com>
2023-11-28 19:12:40 +0100

    final update

--- app.js
+++ app.js
@@ -42,22 +42,36 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// ARRoW TOGGLE FUNCTION * 
-document.addEventListener('DOMContentLoaded', function() {
+// custom focus function for only keyboard interaction excluding mouse events
+// const customButton = document.querySelector('.custom-button');
+
+// customButton.addEventListener('keydown', (event) => {
+//   if (event.code === 'Enter' || event.code === 'Space') {
+//     customButton.classList.add('keyboard-focus');
+//   }
+// });
+
+// customButton.addEventListener('mousedown', () => {
+//   customButton.classList.remove('keyboard-focus');
+// });
+
+document.addEventListener('DOMContentLoaded', function(){
     const upArrow = document.querySelector('.up-arrow');
     const upArrowIcon = document.querySelector('.up-arrow-icon');
-    const contentDiv = document.querySelector('.content-div');
     const taskContents = Array.from(document.querySelectorAll('.task-content'));
 
     let isCollapsed = false;
 
-    upArrow.addEventListener('click', function() {
+    // Toggle function for collapsing/expanding
+    const toggleCollapse = () => {
         isCollapsed = !isCollapsed;
-// if the task content div is not collapsed, show the down icon
+
         if (isCollapsed) {
+            console.log('Collapsing...');
             upArrowIcon.src = 'https://crushingit.tech/hackathon-assets/icon-arrow-down.svg';
             taskContents.forEach(taskContent => {
                 if (!taskContent.nextElementSibling || !taskContent.nextElementSibling.classList.contains('headline')) {
+                    console.log('Collapsing taskContent:', taskContent);
                     taskContent.style.maxHeight = '0';
                     taskContent.style.paddingTop = '0';
                     taskContent.style.paddingBottom = '0';
@@ -66,17 +80,29 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             });
         } else {
-            // else show the up icon
+            console.log('Expanding...');
             upArrowIcon.src = 'https://crushingit.tech/hackathon-assets/icon-arrow-up.svg';
             taskContents.forEach(taskContent => {
+                console.log('Expanding taskContent:', taskContent);
                 taskContent.style.maxHeight = '1000px'; 
                 taskContent.style.paddingTop = '10px'; 
                 taskContent.style.paddingBottom = '10px'; 
                 taskContent.style.overflow = 'visible';
             });
         }
+    };
+
+    // Click event handler
+    upArrow.addEventListener('click', toggleCollapse);
+
+    // Keyup event handler
+    upArrow.addEventListener('keyup', event => {
+        if (event.key === 'Enter') {
+            toggleCollapse();
+        }
     });
 });
+
 
 // ***********************INCREASE LEVEL FUNCTION************
 function increaseAndDecreaseCompletionLevel() {
@@ -101,42 +127,90 @@ function increaseAndDecreaseCompletionLevel() {
     progressUnfilled.style.width = `${100 - progressWidth}%`;
 }
 
+// function to toggle second task
+function toggleSecondTask() {
+    const secondTask = document.querySelectorAll('.second-task');
+    secondTask.classList.toggle('hidden');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const customText = document.querySelector('.custom-text');
+
+    customText.addEventListener('click', toggleSecondTask);
+    customText.addEventListener('keyup', function(event) {
+        if (event.key === 'Enter') {
+            toggleSecondTask();
+        }
+    });
+});
+
 
 
 const cancelSvg = document.querySelector('.cancel-svg');
 const planDiv = document.querySelector('.plan-div');
+const invisible = document.querySelector('.invisibleDiv');
 
-cancelSvg.addEventListener('click', () => {
+// Function to handle the cancellation action
+const cancelAction = () => {
     planDiv.style.display = 'none';
+    invisible.style.display = 'block';
+};
+
+// Click event handler
+cancelSvg.addEventListener('click', cancelAction);
+
+// Enter key event handler
+cancelSvg.addEventListener('keyup', (event) => {
+    // Check if the key pressed is 'Enter' (key code 13)
+    if (event.key === 'Enter') {
+        cancelAction();
+    }
 });
 
 
-function toggleCheckmark(element) {
+
+function toggleCheckmark(element, callback) {
+    const isSpinnerActive = element.classList.contains('spinner-active');
     const isChecked = element.classList.contains('checked');
 
-    if (!isChecked) {
+    if (!isSpinnerActive) {
         element.classList.add('spinner-active'); // Add a class to activate spinner animation
         setTimeout(() => {
             element.classList.remove('spinner-active'); // Hide spinner after 1.5 seconds
-            element.classList.add('checked'); // Show checked class after spinner
+
+            if (!isChecked) {
+                element.classList.add('checked'); // Show checked class after spinner
+            } else {
+                element.classList.remove('checked'); // Hide checked class immediately
+            }
+            
             increaseAndDecreaseCompletionLevel(); // Update completion level after modifying classes
-        }, 1000);
+
+            if (callback) {
+                callback(); // Execute the callback function
+            }
+        }, 700);
     } else {
-        element.classList.remove('checked'); // Hide checked class immediately
-        increaseAndDecreaseCompletionLevel(); // Update completion level after modifying classes
+        if (isChecked) {
+            element.classList.remove('checked'); // Hide checked class immediately
+            element.classList.remove('spinner-active')
+            increaseAndDecreaseCompletionLevel(); // Update completion level after modifying classes
+            if (callback) {
+                callback(); // Execute the callback function
+            }
+        }
     }
 }
 
 
 
 
-
   
     //****************function to toggle each step and close the previous one */ 
-    document.addEventListener('DOMContentLoaded', function() {
+    const toggleTasks = () => {
         const taskContents = document.querySelectorAll('.task-content');
     
-        function collapseAllExcept(clickedDiv) {
+        const collapseAllExcept = (clickedDiv) => {
             taskContents.forEach((taskContent) => {
                 const secondTask = taskContent.querySelector('.second-task');
                 if (taskContent !== clickedDiv.parentElement) {
@@ -147,28 +221,31 @@ function toggleCheckmark(element) {
         }
     
         // *****THIS FUNCTION IS TO COLLAPSE ALL THE OTHER SECOND TASK DIVS IF THE FIRST TASK CLASS IT TOGGLED
-        function toggleContentVisibility(clickedDiv) {
-            const parentTaskContent = clickedDiv.closest('.task-content');
-            const secondTask = parentTaskContent.querySelector('.second-task');
-            const allTaskContents = Array.from(taskContents);
-            const currentIndex = allTaskContents.indexOf(parentTaskContent);
-    
-            collapseAllExcept(clickedDiv);
-    
-            secondTask.style.display = secondTask.style.display === 'none' ? 'flex' : 'none';
-    
-            if (secondTask.style.display === 'none') {
-                clickedDiv.parentElement.classList.add('no-bg-color'); 
-            } else {
-                clickedDiv.parentElement.classList.remove('no-bg-color');
-            }
-    
-            if (currentIndex !== allTaskContents.length - 1 && secondTask.style.display === 'none') {
-                const nextTaskContent = allTaskContents[currentIndex + 1];
-                const nextSecondTask = nextTaskContent.querySelector('.second-task');
-                nextSecondTask.style.display = 'flex';
-                nextTaskContent.classList.remove('no-bg-color');
-            }
+        const toggleContentVisibility = (clickedDiv) => {
+            toggleCheckmark(clickedDiv, () => { 
+                const parentTaskContent = clickedDiv.closest('.task-content');
+                const secondTask = parentTaskContent.querySelector('.second-task');
+                const allTaskContents = Array.from(taskContents);
+                const currentIndex = allTaskContents.indexOf(parentTaskContent);
+        
+                collapseAllExcept(clickedDiv);
+        
+                secondTask.style.display = secondTask.style.display === 'none' ? 'flex' : 'none';
+        
+                if (secondTask.style.display === 'none') {
+                    clickedDiv.parentElement.classList.add('no-bg-color'); 
+                } else {
+                    clickedDiv.parentElement.classList.remove('no-bg-color');
+                }
+        
+                if (currentIndex !== allTaskContents.length - 1 && secondTask.style.display === 'none') {
+                    const nextTaskContent = allTaskContents[currentIndex + 1];
+                    const nextSecondTask = nextTaskContent.querySelector('.second-task');
+                    nextSecondTask.style.display = 'flex';
+                    nextTaskContent.classList.remove('no-bg-color');
+                }
+            })
+           
         }
     
         collapseAllExcept(taskContents[0].querySelector('.first-task'));
@@ -186,6 +263,7 @@ function toggleCheckmark(element) {
                 toggleContentVisibility(this);
             });
         });
-    });
-    
+    }
+        document.addEventListener('DOMContentLoaded', toggleTasks) 
+
     
